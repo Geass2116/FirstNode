6a192f0c0c47b8f126cfae05a694e9e85dd4cdb7
Geass2116 <113455185+Geass2116@users.noreply.github.com>
2023-03-10 22:17:04 +0530

    headache

--- 10-async-offload.js
+++ 10-async-offload.js
@@ -1,11 +1,20 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
 
-    console.log("request event");
-    res.end("hello world")
+//     console.log("request event");
+//     res.end("hello world")
 
+// })
+// server.listen(5000,()=> {
+    //     console.log("Server listening on port : 5000");
+    // })
+    
+    const server = http.createServer();
+    
+    server.on('request',(req,res)=> {
+        res.end("hello world")
+        console.log("request event");
+        
 })
-server.listen(5000,()=> {
-    console.log("Server listening on port : 5000");
-})
+server.listen(5000)
